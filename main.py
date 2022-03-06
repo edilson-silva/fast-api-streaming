bf29757d7a5c88ed9c8ac0633fe087ae8963c56b
Edilson Silva <eddysilva1994@gmail.com>
2022-03-06 18:43:06 -0300

    feat: Add resource to serve file chunks

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import re
 from os import getcwd
 from os import path
 from os import sep
@@ -18,3 +17,24 @@ async def index():
     with open('index.html') as f:
         content = f.read()
         return HTMLResponse(content=content, status_code=200)
+
+@app.get('/videos/{name}')
+async def get_video(name: str, range: str = Header(None)):
+    # Getting video chunks range by "bytes-0-" pattern
+    start, end = range.replace('bytes=', '').split('-')
+    start = int(start)
+    end = int(start + CHUNK_SIZE)
+
+    video_path = f'{VIDEOS_DIR}{name}'
+    video_size = path.getsize(video_path)
+
+    with open(video_path, 'rb') as f:
+        f.seek(start) # Go to specific file point
+        content = f.read(end - start)
+
+        headers = {
+            'Content-Range': f'bytes {start}-{end}/{video_size}',
+            'Accept-Ranges': 'bytes'
+        }
+
+        return Response(content=content, status_code=206, headers=headers, media_type='video/mp4')
